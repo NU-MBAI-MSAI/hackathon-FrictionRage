e1e058eb1ffea298599b02fcc97af8a26ed9a97e
FrictionRage <82208142+FrictionRage@users.noreply.github.com>
2025-09-03 14:56:53 -0500

    Update countinput.py

--- part0/countinput.py
+++ part0/countinput.py
@@ -1,1 +1,14 @@
+def countchars:
+    exclude = {' ', '.', '!', ','}
+    count = 0
+    for ch in st:
+        if ch not in exclude:
+            count += 1
+    return count
+
+
+def main():
+    user_input = input("Enter a string: ")
+    result = countchars(user_input)
+    print(result)
 
