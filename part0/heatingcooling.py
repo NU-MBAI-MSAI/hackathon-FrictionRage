6790bcd1b4ab2c31271b965b9412e95b77910d1e
FrictionRage <82208142+FrictionRage@users.noreply.github.com>
2025-09-03 14:53:06 -0500

    Update heatingcooling.py

--- part0/heatingcooling.py
+++ part0/heatingcooling.py
@@ -1,1 +1,15 @@
+avgTemp = 0
+heatingDays=0
+coolingDays=0
 
+while avgTemp >= -459: 
+  avgTemp=input("Enter the average daily temperature: ")
+  if avgTemp<60:
+    heatingDays += 1
+  else if avgTemp > 80:
+    coolingDays +=1
+
+
+print ("heating days: ",heatingDays)
+print ("cooling days: ",coolingDays)
+    
