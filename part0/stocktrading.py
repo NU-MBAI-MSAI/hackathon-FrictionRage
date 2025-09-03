55305d1d88c68471879059e6125cf2b8946cc576
FrictionRage <82208142+FrictionRage@users.noreply.github.com>
2025-09-03 15:27:05 -0500

    Update stocktrading.py

--- part0/stocktrading.py
+++ part0/stocktrading.py
@@ -1,13 +1,13 @@
 
 def maxProfit(prices):
-maxProfit= 0
-minBuy = 10000000
-
-for i in range(len(prices)):
-    if prices [i] < minBuy: 
-      minBuy = prices[i]
-    if prices[i]-minBuy > maxProfit:
-      maxProfit = prices[i]-minBuy
-
-print("Return value: ", maxProfit)
+    maxProfit= 0
+    minBuy = 10000000
+    
+    for i in range(len(prices)):
+        if prices [i] < minBuy: 
+          minBuy = prices[i]
+        if prices[i]-minBuy > maxProfit:
+          maxProfit = prices[i]-minBuy
+    
+    print("Return value: ", maxProfit)
 
