5770a821b09522aa503ea0116f4788b0a775483c
FrictionRage <82208142+FrictionRage@users.noreply.github.com>
2025-09-03 14:45:13 -0500

    Update speedingticket.py

--- part0/speedingticket.py
+++ part0/speedingticket.py
@@ -1,1 +1,16 @@
 
+speedLimit = input()
+drivingSpeed = input()
+
+diff = drivingSpeed - speedLimit
+
+if diff <= -10:
+  print(50)
+else if diff >= 6 && diff <= 20:
+  print(75)
+else if diff >=21 && diff <= 40:
+  print(100)
+else if diff >= 41:
+  print(300)
+else:
+  print(0)
