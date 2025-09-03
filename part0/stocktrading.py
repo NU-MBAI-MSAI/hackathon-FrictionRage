ebbf5544b586addf8a6b99975155ea1617ea9250
FrictionRage <82208142+FrictionRage@users.noreply.github.com>
2025-09-03 15:27:22 -0500

    Update stocktrading.py

--- part0/stocktrading.py
+++ part0/stocktrading.py
@@ -4,7 +4,7 @@ def maxProfit(prices):
     minBuy = 10000000
     
     for i in range(len(prices)):
-        if prices [i] < minBuy: 
+        if prices[i] < minBuy: 
           minBuy = prices[i]
         if prices[i]-minBuy > maxProfit:
           maxProfit = prices[i]-minBuy
